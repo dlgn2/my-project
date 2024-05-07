47178e28293d0c89c37feeccd2a11b470c06f258
Enes Dolgun <58950702+dlgn2@users.noreply.github.com>
2024-05-07 17:02:36 +0300

    Update route.tsx

--- app/frames/route.tsx
+++ app/frames/route.tsx
@@ -2,19 +2,29 @@ import { Button } from "frames.js/next";
 import { frames } from "./frames";
 
 const handleRequest = frames(async (ctx) => {
+  let responseMessage = "Click a button below to vote:";
+  if (ctx.pressedButton) {
+    responseMessage = `Thank you for voting ${ctx.searchParams.value}!`;
+    // backend api call to store vote
+  }
+
   return {
-    image: (
-      <span>
-        {ctx.pressedButton
-          ? `You clicked ${ctx.searchParams.value}`
-          : "Click a button below"}
-      </span>
-    ),
+    image: <span>{responseMessage}</span>,
     buttons: [
-      <Button key="yes" action="post" target={{ query: { value: "Yes" } }}>
+      <Button
+        key="yes"
+        action="post"
+        target={{ query: { value: "Yes" } }}
+        aria-label="Vote Yes"
+      >
         Yes
       </Button>,
-      <Button key="no" action="post" target={{ query: { value: "No" } }}>
+      <Button
+        key="no"
+        action="post"
+        target={{ query: { value: "No" } }}
+        aria-label="Vote No"
+      >
         No
       </Button>,
     ],
